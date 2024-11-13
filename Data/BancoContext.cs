2f90ddb7dedcb45c9d911d23e38851a0eca2141d
leonardopereira <leonardopereira@bne-empregos.com.br>
2024-11-13 08:44:39 -0300

    Primeiro commit

---
+++ Data/BancoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Blazor.Model;
+using System;
+
+namespace MVC_Blazor.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios1 { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Inscricao> Inscricoes { get; set; } 
+    }
+}
